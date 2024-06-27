b277aa39e9e99aed0d73961a43458c6ced67fcd2
Vuek0 <vueko.eruko@gmail.com>
2024-06-27 14:54:05 +0500

    done

--- public/main.js
+++ public/main.js
@@ -133,85 +133,112 @@ if(signUpForm){
         }
     })
 }
-const headerLinks = document.querySelectorAll('.menu__item a');
+const headerLinks = document.querySelectorAll(".menu__item a");
 const pill = document.querySelector(".transition__image");
 const html = document.querySelector("html");
-const forms = document.querySelectorAll(".form__container form")
-function removePixels(value){
-    return value.split('px')[0];
-
+const forms = document.querySelectorAll(".form__container form");
+const bannerText = document.querySelector(".banner__text");
+const bannerImage = document.querySelector(".banner__image");
+const cards = document.querySelectorAll(".card");
+function removePixels(value) {
+  return value.split("px")[0];
 }
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    const animation = localStorage.getItem("animation");
-    const object = JSON.parse(animation)
-    forms.forEach(item => {
-        item.style.opacity = 1;
-        item.style.transform = "translate(0px)";
-    })
-    if(animation){ 
-        html.style.overflowY = "hidden";
-        console.log(object.rotate);
-        console.log(object.translate);
-        let rotateValue = object.rotate;
-        let translateValue = object.translate;
-        localStorage.removeItem('animation');
-        // pill.style.display = "flex";
-        // pill.style.transform = `translateX(${translateValue}px)`
+document.addEventListener("DOMContentLoaded", () => {
+  const animation = localStorage.getItem("animation");
+  const object = JSON.parse(animation);
+  forms.forEach((item) => {
+    item.style.opacity = 1;
+    item.style.transform = "translate(0px)";
+  });
+  if (bannerText && bannerImage && cards) {
+    cards.forEach((item) => {
+      item.style.opacity = 1;
+      item.style.transform = "translate(0px)";
+    });
+    bannerText.style.opacity = 1;
+    bannerText.style.transform = "translate(0px)";
+    bannerImage.style.opacity = 1;
+    bannerImage.style.transform = "translate(0px)";
+  }
 
-        const anim = setInterval(()=>{
-            translateValue+=23;
-            rotateValue+=3;
-            pill.style.transform = `translateX(${translateValue}px) rotate(${rotateValue}deg)`
-            // pill.style.left = `${removePixels(pill.style.left) - 10}px`
-        }, 15)
-        setTimeout(()=>{
-            pill.style.opacity = 0;
-            pill.style.display = "none";
-            pill.style.transform = "translateX(-2000px)";
-            // pill.style.opacity = 1;
-            html.style.overflowY = "auto";
-            clearInterval(anim);            
-        }, 1300)
-    } else{
-        pill.style.display = "none";
-        pill.style.transform = "translateX(-2000px)"
-    }
-})
+  if (animation) {
+    html.style.overflowY = "hidden";
+    console.log(object.rotate);
+    console.log(object.translate);
+    let rotateValue = object.rotate;
+    let translateValue = object.translate;
+    localStorage.removeItem("animation");
+    // pill.style.display = "flex";
+    // pill.style.transform = `translateX(${translateValue}px)`
+
+    const anim = setInterval(() => {
+      translateValue += 23;
+      rotateValue += 3;
+      pill.style.transform = `translateX(${translateValue}px) rotate(${rotateValue}deg)`;
+      // pill.style.left = `${removePixels(pill.style.left) - 10}px`
+    }, 15);
+    setTimeout(() => {
+      pill.style.opacity = 0;
+      pill.style.display = "none";
+      pill.style.transform = "translateX(-2000px)";
+      // pill.style.opacity = 1;
+      html.style.overflowY = "auto";
+      clearInterval(anim);
+    }, 1300);
+  } else {
+    pill.style.display = "none";
+    pill.style.transform = "translateX(-2000px)";
+  }
+});
 
 headerLinks.forEach((item) => {
-    item.addEventListener("click", function animationHandler(e){
-        e.preventDefault();
-        popup.style.opacity = 0;
-        forms.forEach(form => {
-            form.style.opacity = 0;
-            form.style.transform = "translateY(150px)";
-        })
+  item.addEventListener("click", function animationHandler(e) {
+    e.preventDefault();
+    popup.style.opacity = 0;
+    forms.forEach((form) => {
+      form.style.opacity = 0;
+      form.style.transform = "translateY(150px)";
+    });
 
-        item.removeEventListener("click", animationHandler);
-        item.addEventListener("click", ()=>{
-            e.preventDefault();
+    if (bannerText && bannerImage && cards) {
+      cards.forEach((item) => {
+        item.style.opacity = 0;
+        item.style.transform = "translate(180px)";
+      });
+      bannerText.style.opacity = 0;
+      bannerText.style.transform = "translate(150px)";
+      bannerImage.style.opacity = 0;
+      bannerImage.style.transform = "translate(150px)";
+    }
+
+    item.removeEventListener("click", animationHandler);
+    item.addEventListener("click", () => {
+      e.preventDefault();
+    });
+    html.style.overflowY = "hidden";
+    let rotateValue = 0;
+    let translateValue = -1300;
+    const href = item.getAttribute("href");
+    // console.log(href);
+    pill.style.display = "flex";
+    pill.style.opacity = 1;
+    const anim = setInterval(() => {
+      translateValue += 20;
+      rotateValue += 3;
+      pill.style.transform = `translateX(${translateValue}px) rotate(${rotateValue}deg)`;
+      // pill.style.left = `${removePixels(pill.style.left) - 10}px`
+    }, 15);
+    setTimeout(() => {
+      clearInterval(anim);
+      localStorage.setItem(
+        "animation",
+        JSON.stringify({
+          translate: translateValue,
+          rotate: rotateValue,
         })
-        html.style.overflowY = "hidden";
-        let rotateValue = 0;
-        let translateValue = -1300;
-        const href = item.getAttribute('href');
-        // console.log(href);
-        pill.style.display = "flex";
-        pill.style.opacity = 1;
-        const anim = setInterval(()=>{
-            translateValue+=20;
-            rotateValue+=3;
-            pill.style.transform = `translateX(${translateValue}px) rotate(${rotateValue}deg)`
-            // pill.style.left = `${removePixels(pill.style.left) - 10}px`
-        }, 15)
-        setTimeout(()=>{
-            clearInterval(anim);
-            localStorage.setItem("animation", JSON.stringify({
-                translate: translateValue,
-                rotate: rotateValue
-            }));            
-            window.location.href = href;
-        }, 1160)
-    })
-})
+      );
+      window.location.href = href;
+    }, 1160);
+  });
+});
